ad14cca66f80fd5663635cc081f6d3c5782188a0
keigomichi <hundredacrewood1165g7@gmail.com>
2023-01-29 02:21:43 +0900

    feat: Task に不足したプロパティを追加

--- app/src/main/java/com/example/capturethetask/model/Task.kt
+++ app/src/main/java/com/example/capturethetask/model/Task.kt
@@ -10,5 +10,7 @@ data class Task(
     @PrimaryKey @ColumnInfo(name = "id") val id: String = UUID.randomUUID().toString(),
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "description") val description: String,
-    @ColumnInfo(name = "completed") val isCompleted: Boolean
+    @ColumnInfo(name = "completed") val isCompleted: Boolean,
+    @ColumnInfo(name = "starred") val isStarred: Boolean,
+    @ColumnInfo(name = "filepath") val filePath: String
 )
