c3f3c12ab1b07ca096dfcd1572cfbbbcaccdd095
notchcoder <kokiuaxdx@gmail.com>
2023-01-29 14:53:56 +0900

    feat: カラーパレットの変更

--- app/src/main/java/com/example/capturethetask/ui/theme/Color.kt
+++ app/src/main/java/com/example/capturethetask/ui/theme/Color.kt
@@ -2,7 +2,11 @@ package com.example.capturethetask.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val DarkPrimary = Color(0xFF94CCFF)
+val DarkPrimaryVariant = Color(0xFF004A74)
+val DarkSecondary = Color(0xFFB5C9DE)
+val LightPrimary = Color(0xFF006399)
+val LightPrimaryVariant = Color(0xFFCDE5FF)
+val LightSecondary = Color(0xFF4E6072)
+
+val StarColor = Color(0xFFD8CA00)
